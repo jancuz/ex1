df15a46838718c43fec57a724618818190f17e55
jancuz <jancuz@yandex.ru>
2018-06-07 21:06:48 +0500

    Добавлен класс для представления данных о вершине прямоугольника

---
+++ ex1/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int a, int b)
+        {
+            X = a; Y = b;
+        }
+    }
+}
